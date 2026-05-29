970f6d691da2a110fb9728afac6bac423c5eac60
Izzah-nadeem <izzahnadeem43@gmail.com>
2026-05-29 11:44:40 +0500

    Update supabase.js

--- web/lib/supabase.js
+++ web/lib/supabase.js
@@ -1,8 +1,8 @@
-```js
+js
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
-```
+
